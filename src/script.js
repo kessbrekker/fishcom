5810be02a1628e39fbf59e5c9c3f2821e00c8972
Kess <talhasaidbsr@gmail.com>
2025-10-02 11:05:08 +0300

    #6

--- src/script.js
+++ src/script.js
@@ -35,3 +35,17 @@ if(navbarList){
     });
   });
 }
+
+document.querySelectorAll('.favorite-icon').forEach(function(icon){
+  icon.addEventListener('click', function(){
+    if(this.classList.contains('fa-regular')){
+      this.classList.remove('fa-regular');
+      this.classList.add('fa-solid');
+      this.style.color = 'var(--fish-red)';
+    } else {
+      this.classList.remove('fa-solid');
+      this.classList.add('fa-regular');
+      this.style.color = '';
+    }
+  });
+});
